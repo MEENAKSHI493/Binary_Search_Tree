7546e3495d512de5b99340a5c2096d9ff4e7144c
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-05 01:22:25 +0530

    Create Binary Tree Stucture

--- com/Bridgelabz/MyBinaryTree.java
+++ com/Bridgelabz/MyBinaryTree.java
@@ -3,17 +3,30 @@ package com.Bridgelabz;
 public class MyBinaryTree {
 
 	public static void main(String[] args)  
+
 		    { 
-			       //create a BST object
-			    	BinarySearchTree bst = new BinarySearchTree(); 
-			        
-			        //insert data into BST
-			        bst.insert(56); 
-			        bst.insert(30); 
-			        bst.insert(70); 
-			       
-			        //print the BST
-			        System.out.println("The BST Created with input data(Left-root-right):"); 
-			        bst.inorder();		    
-}
+		       //create a BST object
+		    	BinarySearchTree bst = new BinarySearchTree(); 
+		        
+		        //insert data into BST
+		        bst.insert(56); 
+		        bst.insert(30); 
+		        bst.insert(70);
+		        bst.insert(22); 
+		        bst.insert(40); 
+		        bst.insert(11);
+		        bst.insert(3); 
+		        bst.insert(16); 
+		        bst.insert(60);
+		        bst.insert(95);
+		        bst.insert(65);
+		        bst.insert(63);
+		        bst.insert(67);
+		       
+		        //print the BST
+		        System.out.println("The BST Created with input data(Left-root-right):"); 
+		        bst.inorder(); 
+		           
+		     } 
+		
 	}
