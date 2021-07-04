96b7c0b591beb0b1e4b9f7e7a5276dc7c3b40684
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-05 00:51:58 +0530

    Create Binary Tree

--- com/Bridgelabz/MyBinaryTree.java
+++ com/Bridgelabz/MyBinaryTree.java
@@ -2,9 +2,18 @@ package com.Bridgelabz;
 
 public class MyBinaryTree {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static void main(String[] args)  
+		    { 
+			       //create a BST object
+			    	BinarySearchTree bst = new BinarySearchTree(); 
+			        
+			        //insert data into BST
+			        bst.insert(56); 
+			        bst.insert(30); 
+			        bst.insert(70); 
+			       
+			        //print the BST
+			        System.out.println("The BST Created with input data(Left-root-right):"); 
+			        bst.inorder();		    
 }
+	}
